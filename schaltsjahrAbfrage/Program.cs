9943eef9ce1bcbfeaa087e50de2e3b7789f186e7
Fly <lucashuber667@gmail.com>
2025-09-04 13:21:58 +0000

    Added

---
+++ schaltsjahrAbfrage/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MeinCSharpProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geben sie das jahr ein für die abfrage");
+            int jahr = Convert.ToInt32(Console.ReadLine());
+
+            if (jahr % 4 == 0)
+            {
+                
+                if (jahr % 100 == 0)
+                {
+                     System.Console.WriteLine(jahr + " ist kein schaltjahr");
+                }
+                else
+                {
+                    System.Console.WriteLine(jahr + " ist kein schaltjahr");
+                    if (jahr % 400 == 0)
+                    {
+                        System.Console.WriteLine(jahr + " ist ein schaltjahr");
+                    }
+                    else
+                    {
+
+                        System.Console.WriteLine(jahr + " ist kein schaltjahr");
+                
+                    }
+                   
+
+                }
+                    
+            }
+            else
+            {
+
+                System.Console.WriteLine(jahr + " ist kein schaltjahr");
+
+            }
+            
+           
+            
+            Console.ReadLine();
+        }
+    }
+}
